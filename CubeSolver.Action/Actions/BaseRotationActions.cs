ea6e26c3e94b212f7a572274bae5b4270d44cfd4
Tiantsoa Sh <tiantsoa.sh@gmail.com>
2018-12-21 16:55:36 +0400

    Rotation adjacent side

--- CubeSolver.Action/Actions/BaseRotationActions.cs
+++ CubeSolver.Action/Actions/BaseRotationActions.cs
@@ -2,6 +2,7 @@
 using CubeSolver.Domain;
 using CubeSolver.Infrastructure.Enums;
 using CubeSolver.Infrastructure.Extensions;
+using System;
 using System.Collections.Generic;
 
 namespace CubeSolver.Action.Actions
@@ -9,6 +10,12 @@ namespace CubeSolver.Action.Actions
     public abstract class BaseRotationActions: BaseDoubleLayerRotationActions
     {
         public void Rotate(Cube cube, RotationSense rotationSense, PickType pickType, int index)
+        {
+            RotateLayer(cube, rotationSense, pickType, index);
+            RotateAdjacentSide(cube, rotationSense, pickType, index);
+        }
+
+        protected void RotateLayer(Cube cube, RotationSense rotationSense, PickType pickType, int index)
         {
             FacePosition currentFacePosition = FacePosition.Front;
             FacePosition nextFacePosition = currentFacePosition.Rotate(rotationSense, pickType);
@@ -40,7 +47,18 @@ namespace CubeSolver.Action.Actions
             } while (nextFacePosition != endPosition);
         }
 
-        private FacePosition GetEndingRotation(RotationSense rotationSense, PickType pickType)
+        protected void RotateAdjacentSide(Cube cube, RotationSense rotationSense, PickType pickType, int index)
+        {
+            if (index == 2)
+                return;
+
+            Face adjacentFace = cube.GetFaceByPosition(GetAdjacentFace(pickType, index));
+
+            RotateMiddlePieces(adjacentFace, rotationSense);
+            RotateCornerPieces(adjacentFace, rotationSense);
+        }
+
+        protected FacePosition GetEndingRotation(RotationSense rotationSense, PickType pickType)
         {
             switch (pickType)
             {
@@ -51,6 +69,38 @@ namespace CubeSolver.Action.Actions
             }
 
             return default(FacePosition);
+        }
+
+        protected FacePosition GetAdjacentFace(PickType pickType, int index)
+        {
+            switch (pickType)
+            {
+                case PickType.Horizontal:
+                    if (index == 1)
+                        return FacePosition.Up;
+                    if (index == 3)
+                        return FacePosition.Down;
+                    break;
+
+                case PickType.Vertical:
+                    if (index == 1)
+                        return FacePosition.Left;
+                    if (index == 3)
+                        return FacePosition.Right;
+                    break;
+            }
+
+            throw new Exception("Face adjacente inexistante");
+        }
+
+        protected void RotateMiddlePieces(Face face, RotationSense rotationSense)
+        {
+            
+        }
+
+        protected void RotateCornerPieces(Face face, RotationSense rotationSense)
+        {
+
         }
     }
 }
